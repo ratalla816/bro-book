f0f5dd2c73e942f38b79309de284ff73d6e5f8c2
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-10-23 20:26:03 -0500

    user model added

--- models/User.js
+++ models/User.js
@@ -1,5 +1,44 @@
 const { Schema, model } = require("mongoose");
 
-c
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: "username is required",
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: "Email address is required",
+      unique: true,
+      match: [/.+@.+\..+/, "Please fill a valid email address"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("User", UserSchema);
 
 module.exports = User;
