310464660ba816a690e5e9ffdfc34d824d7bd260
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-10-24 22:18:06 -0500

    updated controllers

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -13,6 +13,7 @@ const thoughtController = {
   // GET all thoughts
   getAllThoughts(req, res) {
     Thought.find({})
+
       .select("-__v")
       .sort({ createdAt: -1 })
       .then((dbThoughtData) => res.json(dbThoughtData))
@@ -27,9 +28,9 @@ const thoughtController = {
     Thought.findOne({ _id: req.params.thoughtId })
       .then((dbThoughtData) => {
         if (!dbThoughtData) {
+
           return res
-            .status(404)
-            .json({ message: "If you're here, and I'm here, doesn't it make it OUR time?" });
+            .status(404).json({ message: "If you're here, and I'm here, doesn't it make it OUR time?" });
         }
         res.json(dbThoughtData);
       })
@@ -43,6 +44,7 @@ const thoughtController = {
   addThought({ params, body }, res) {
     console.log(body);
     Thought.create(body)
+
       .then(({ _id }) => {
         return User.findOneAndUpdate(
           { _id: params.userId },
@@ -50,6 +52,7 @@ const thoughtController = {
           { new: true }
         );
       })
+
       .then((dbUserData) => {
         if (!dbUserData) {
           res.status(404).json({ message: "NO WAY!" });
@@ -57,16 +60,19 @@ const thoughtController = {
         }
         res.json(dbUserData);
       })
+
       .catch((err) => res.json(err));
   },
 
   // 'PUT' update a thought by it's ID
   updateThought(req, res) {
-    Thought.findOneAndUpdate(
+    Thought.findOneAndUpdate
+    (
       { _id: req.params.thoughtId },
       { $set: req.body },
       { new: true, runValidators: true }
     )
+
       .then((dbThoughtData) => {
         if (!dbThoughtData) {
           return res
@@ -75,6 +81,7 @@ const thoughtController = {
         }
         res.json(dbThoughtData);
       })
+
       .catch((err) => {
         console.log(err);
         res.status(400).json(err);
@@ -85,40 +92,48 @@ const thoughtController = {
   removeThought({ params }, res) {
     Thought.findOneAndDelete({ _id: params.thoughtId })
       .then((deletedThought) => {
+
         if (!deletedThought) {
           return res.status(404).json({ message: "Pitted, so pitted!" });
+
         }
-        return User.findOneAndUpdate(
+        return User.findOneAndUpdate
+        (
           { _id: params.userId },
           { $pull: { thoughts: params.commentId } },
           { new: true }
         );
       })
+
       .then((dbUserData) => {
         if (!dbUserData) {
+
           res.status(404).json({ message: "That's not your dude, Bro!" });
           return;
         }
         res.json(dbUserData);
       })
+
       .catch((err) => res.json(err));
   },
 
   // POST a reaction to a thought
   addReaction(req, res) {
-    Thought.findOneAndUpdate(
+    Thought.findOneAndUpdate
+    (
       { _id: req.params.thoughtId },
       { $addToSet: { reactions: req.body } },
       { new: true, runValidators: true }
     )
+
       .then((dbThoughtData) => {
         if (!dbThoughtData) {
           return res
-            .status(404)
-            .json({ message: "Bieber!!!" });
+            .status(404).json({ message: "Bieber!!!" });
         }
         res.json(dbThoughtData);
       })
+
       .catch((err) => {
         console.log(err);
         res.status(400).json(err);
@@ -127,11 +142,13 @@ const thoughtController = {
 
   // DELETE a reaction by it's ID
   removeReaction(req, res) {
-    Thought.findOneAndUpdate(
+    Thought.findOneAndUpdate
+    (
       { _id: req.params.thoughtId },
       { $pull: { reactions: { reactionId: req.params.reactionId } } },
       { new: true, runValidators: true }
     )
+
       .then((dbThoughtData) => {
         if (!dbThoughtData) {
           return res
@@ -140,6 +157,7 @@ const thoughtController = {
         }
         res.json(dbThoughtData);
       })
+      
       .catch((err) => {
         console.log(err);
         res.status(400).json(err);
